e00bcf749f773e615fd34bf0504f98daa008ab57
Woo <jhsw0375@gmail.com>
2018-08-11 01:09:08 +0900

    BOJ 10950 solve

---
+++ src/입출력/BOJ10950.java
@@ -0,0 +1,19 @@
+package 입출력;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ10950 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int test_case = Integer.parseInt(bf.readLine());
+        for (int i = 0; i < test_case; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(a + b);
+        }
+    }
+}
