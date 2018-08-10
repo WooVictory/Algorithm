08206be6bfe1c3366d75da4e29895b3b095167b3
Woo <jhsw0375@gmail.com>
2018-08-11 01:03:48 +0900

    BOJ 2558 solve

---
+++ src/입출력/BOJ2558.java
@@ -0,0 +1,14 @@
+package 입출력;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2558 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(bf.readLine());
+        int b = Integer.parseInt(bf.readLine());
+        System.out.println(a+b);
+    }
+}
