d231ee5e4bb15a7808c91407419b3c2b42b2468a
Woo <jhsw0375@gmail.com>
2018-08-11 01:24:44 +0900

    BOJ 10951 solve

---
+++ src/입출력/BOJ10951.java
@@ -0,0 +1,15 @@
+package 입출력;
+
+import java.util.Scanner;
+
+public class BOJ10951 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+
+    }
+}
