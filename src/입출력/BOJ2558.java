8541e721a665556271ff7d3a5a37f5c1af0eb651
Woo <jhsw0375@gmail.com>
2018-08-10 18:37:04 +0900

    BOJ 1000, 10950, 2558 solve

--- src/입출력/BOJ2558.java
+++ src/입출력/BOJ2558.java
@@ -3,14 +3,13 @@ package 입출력;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-public class BOJ1000 {
+public class BOJ2558 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine()," ");
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(bf.readLine());
+        int b = Integer.parseInt(bf.readLine());
         System.out.println(a+b);
+
     }
 }
