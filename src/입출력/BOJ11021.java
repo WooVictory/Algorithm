7bc48192c15322ce1ef84a2937ca828cad970716
Woo <jhsw0375@gmail.com>
2018-08-12 00:01:37 +0900

    BOJ 11021 solve

---
+++ src/입출력/BOJ11021.java
@@ -0,0 +1,21 @@
+package 입출력;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ11021 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int test_case = Integer.parseInt(bf.readLine());
+        for (int i = 0; i < test_case; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println("Case #" + (i + 1) + ": " + (a + b));
+        }
+
+    }
+}
