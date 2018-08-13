33527ad55b3091dab6bbd12b4762c541eb666e56
Woo <jhsw0375@gmail.com>
2018-08-13 18:53:18 +0900

    BOJ 11719 solve

---
+++ src/입출력/BOJ11719.java
@@ -0,0 +1,13 @@
+package 입출력;
+
+import java.util.Scanner;
+
+public class BOJ11719 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+            System.out.println(sc.nextLine());
+        }
+    }
+
+}
