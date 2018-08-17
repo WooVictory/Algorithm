de636a115ccba2013bda7c86c09fa9f576a71e37
Woo <jhsw0375@gmail.com>
2018-08-17 16:38:46 +0900

    BOJ 10828 solve

--- src/자료구조/Stack/BOJ10828.java
+++ src/자료구조/Stack/BOJ10828.java
@@ -1,4 +1,45 @@
 package 자료구조.Stack;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class BOJ10828 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Stack stack = new Stack();
+        int test_case = sc.nextInt();
+        for(int i=0;i<test_case;i++){
+            String command = sc.next();
+            switch (command){
+                case "push":
+                    int num = sc.nextInt();
+                    stack.push(num);
+                    break;
+                case "pop":
+                    if(stack.isEmpty()){
+                        System.out.println(-1);
+                    }else {
+                        System.out.println(stack.pop());
+                    }
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    if(stack.isEmpty()){
+                        System.out.println(1);
+                    }else{
+                        System.out.println(0);
+                    }
+                    break;
+                case "top":
+                    if(stack.isEmpty()){
+                        System.out.println(-1);
+                    }else{
+                        System.out.println(stack.peek());
+                    }
+                    break;
+            }
+        }
+    }
 }
