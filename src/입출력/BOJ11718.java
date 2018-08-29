2151f09167737c189059b0364de34b67cfe91ae1
Woo <jhsw0375@gmail.com>
2018-08-29 23:42:33 +0900

    Modify Code

--- src/입출력/BOJ11718.java
+++ src/입출력/BOJ11718.java
@@ -5,10 +5,8 @@ import java.util.Scanner;
 public class BOJ11718 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) { // EOF까지 입력받기 위해 sc.hasNext() 사용
+        while (sc.hasNext()){
             System.out.println(sc.nextLine());
-            // 공백을 포함한 한 줄을 입력받기 위해 sc.nextLine() 사용
-            // sc.next() : 공백을 포함하지 않고 입력을 받음 즉, 공백을 입력하면 거기서 짤림
         }
     }
 }
--- src/입출력/BOJ11721.java
+++ src/입출력/BOJ11721.java
@@ -17,5 +17,8 @@ public class BOJ11721 {
 
         }
 
+
+
+
     }
 }
