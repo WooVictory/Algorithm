a6fe97e8de41586ec538db5318739d4785b648d2
Woo <jhsw0375@gmail.com>
2018-09-22 18:04:53 +0900

    BOJ 11816 solve

---
+++ src/입출력/BOJ11816.java
@@ -0,0 +1,11 @@
+package 입출력;
+
+import java.util.Scanner;
+
+public class BOJ11816 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.printf("%i",num);
+    }
+}
