654926de74b255f9fec09d02fef680db984e440f
Woo <jhsw0375@gmail.com>
2018-10-03 16:44:12 +0900

    BOJ 1076 modify code

--- src/language/BOJ1076.java
+++ src/language/BOJ1076.java
@@ -3,12 +3,14 @@ package language;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.nio.Buffer;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BOJ1076 {
     public static void main(String[] args) throws IOException {
+        // 저항 문제는 Map 자료구조를 사용해서 풀 수 있다.
+        // 문제에서 입력은 세 개이므로 for문을 돌려서 받지 않고 그냥 한번에 받는 것이 더 간단.
+
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         Map<String, Integer> map = new HashMap<>();
 
@@ -23,31 +25,15 @@ public class BOJ1076 {
         map.put("grey", 8);
         map.put("white", 9);
 
-        StringBuilder sb= new StringBuilder();
-
         String one = bf.readLine(), two = bf.readLine(), three = bf.readLine();
 
-        int first = map.get(one)*10+map.get(two);
-        System.out.println(first * (long)Math.pow(10, map.get(three).intValue()));
+
+        int firstResult = map.get(one) * 10 + map.get(two);
+        long secondResult = (long) Math.pow(10, map.get(three));
+        System.out.println(firstResult * secondResult);
 
 
     }
 }
 
-class Data {
-    private int value;
-    private int multiple;
 
-    public Data(int value, int multiple) {
-        this.value = value;
-        this.multiple = multiple;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public int getMultiple() {
-        return multiple;
-    }
-}
