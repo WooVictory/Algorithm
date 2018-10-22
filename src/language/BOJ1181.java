dc4d25200346d96502fcd55a2d1c5a12051a1571
Woo <jhsw0375@gmail.com>
2018-10-22 23:10:10 +0900

    BOJ 1181 solve

---
+++ src/language/BOJ1181.java
@@ -0,0 +1,50 @@
+package language;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ1181 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        int N = Integer.parseInt(bf.readLine());
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            String input = bf.readLine();
+            set.add(input);
+
+        }
+
+        List<String> list = new ArrayList<>(set);
+
+        // 일단 사전순으로 먼저 정렬
+        Collections.sort(list);
+
+        // 길이순으로 정렬
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() > o2.length())
+                    return 1;
+                else if (o1.length() < o2.length())
+                    return -1;
+                else
+                    return 0;
+            }
+        });
+
+
+        // 출력
+        for (int i = 0; i < list.size(); i++)
+            bw.write(list.get(i) + "\n");
+
+        bw.flush();
+        bw.close();
+        bf.close();
+
+
+    }
+
+}
