aa06a15a28b06762b88a6450ab7fc3567ce3684d
Woo <jhsw0375@gmail.com>
2019-03-24 00:42:12 +0900

    선택 정렬 구현하기

---
+++ src/정렬/SelectSort.java
@@ -0,0 +1,41 @@
+package 정렬;
+
+/**
+ * created by victory_woo on 20/03/2019
+ * 60000개 랜덤한 숫자를 선택 정렬로 정렬한 경우 : 6012 걸림
+ */
+public class SelectSort {
+    public static void main(String[] args) {
+        //int[] arr = {254, 3, 213, 64, 75, 56, 4, 324, 65, 78, 9, 5, 76, 3410, 8, 342, 76};
+
+        int[] arr = new int[60000];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 60000);
+        }
+
+        long start_time = System.currentTimeMillis();
+        // SelectSort
+        selectionSort(arr);
+        long end_time = System.currentTimeMillis();
+        System.out.println("선택 정렬 실행 시간 : " + (end_time - start_time));
+
+
+    }
+
+    private static void selectionSort(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = tmp;
+                }
+            }
+        }
+
+    }
+
+
+}
